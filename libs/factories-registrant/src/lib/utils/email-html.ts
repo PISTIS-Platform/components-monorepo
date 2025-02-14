8bc205ec916b3b1b979a7aa19cb98200cfcc0b28
Hermes J. Pappas <hjp@hjp.gr>
2025-02-14 12:56:27 +0200

    Create constants for display in email

--- libs/factories-registrant/src/lib/utils/email-html.ts
+++ libs/factories-registrant/src/lib/utils/email-html.ts
@@ -1,4 +1,10 @@
 import { FactoryCreationDTO } from '../dto/factory-creation.dto';
+import {
+    countriesInEnglish,
+    domainsInEnglish,
+    typesInEnglish,
+    companySizesInEnglish,
+} from '../constants/factory-creation';
 
 export const htmlForEmail = (data: FactoryCreationDTO): string => {
     return `    
@@ -10,10 +16,10 @@ export const htmlForEmail = (data: FactoryCreationDTO): string => {
     <p>Here are the details of your factory:</p>
     <ul>
       <li>Organization Name: <b>${data.organizationName}</b></li>
-      <li>Organization Type: <b>${data.type}</b></li>
-      <li>Organization Domain: <b>${data.domain}</b></li>
-      <li>Organization Country: <b>${data.country}</b></li>
-      <li>Organization Size: <b>${data.size}</b></li>
+      <li>Organization Type: <b>${typesInEnglish[data.type]}</b></li>
+      <li>Organization Domain: <b>${domainsInEnglish[data.domain]}</b></li>
+      <li>Organization Country: <b>${countriesInEnglish[data.country]}</b></li>
+      <li>Organization Size: <b>${companySizesInEnglish[data.size]}</b></li>
       <li>Factory URL: <a href="https://${data.factoryPrefix}.pistis-market.eu">https://${
         data.factoryPrefix
     }.pistis-market.eu</a></li>
