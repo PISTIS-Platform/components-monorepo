215560698d298c84947a7d0baea7879a7abdbf2c
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2025-06-17 11:49:45 +0300

    Change access_url in metadata

--- libs/provider/src/lib/provider.service.ts
+++ libs/provider/src/lib/provider.service.ts
@@ -177,7 +177,7 @@ export class ProviderService {
             distributions: [
                 {
                     title: { en: 'Streaming Data Distribution' },
-                    access_url: [`http://develop.pistis-market.eu:9092`],
+                    access_url: [`http://kafka.${factory.factoryPrefix}.pistis-market.eu:9092`],
                     format: { resource: 'Kafka-stream' },
                     byte_size: '0',
                 },
