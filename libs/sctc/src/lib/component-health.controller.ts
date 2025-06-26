de2d86f807ab3460980528c45eb668492971f0e3
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2025-06-26 12:54:07 +0300

    Revert health endpoint to public access

--- libs/sctc/src/lib/component-health.controller.ts
+++ libs/sctc/src/lib/component-health.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { Public } from 'nest-keycloak-connect';
 
 @Controller('health')
 export class ComponentHealthController {
     constructor(private health: HealthCheckService) {}
 
     @Get()
+    @Public()
     @HealthCheck()
     async check() {
         return this.health.check([() => ({ sctcComponent: { status: 'up', message: 'ready to serve' } })]);
