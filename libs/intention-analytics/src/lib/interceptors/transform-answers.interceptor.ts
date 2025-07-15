c0a357b2871425b701be52a09f42a199d906848f
Hermes J. Pappas <hjp@hjp.gr>
2025-07-15 15:57:42 +0300

    Delete console log, add comments for changes

--- libs/intention-analytics/src/lib/interceptors/transform-answers.interceptor.ts
+++ libs/intention-analytics/src/lib/interceptors/transform-answers.interceptor.ts
@@ -17,6 +17,7 @@ export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]>
                 const finalResults: QuestionResponse[] = [];
 
                 //put all the question responses into one array
+                //add the date it was given at in order to group by answer or by date
                 const allResponses: Record<string, any>[] = [].concat(
                     ...data.map((answerRow: IAnswer) =>
                         answerRow.responses.map((response: any) => ({
@@ -25,8 +26,6 @@ export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]>
                         })),
                     ),
                 );
-
-                console.log({ allResponses });
 
                 // based on the above, group questions into the final results array
                 for (let i = 0; i < allResponses.length; i++) {
@@ -38,6 +37,7 @@ export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]>
                     ) as QuestionResponse;
 
                     //create array with submitted answers by user (text or option)
+                    //add date to each answer in order to group by answer or by date
                     const submittedAnswers: { response: string[]; date: string }[] =
                         'text' in questionResponse && questionResponse['text']
                             ? [{ response: questionResponse['text'], date: questionResponse['date'] }]
