7a3668950da49f28412f777b5a2a206dabd4291c
Hermes J. Pappas <hjp@hjp.gr>
2025-07-25 14:50:52 +0300

    feat: intentions analytics display (#26)
    * Alter interceptor to include question type
    
    * Add date to answers in order to later display graphs
    
    * Delete console log, add comments for changes
    
    * Include options
    
    * Make options simple strings since description is not used
    
    * Add creator id to returned fields
    
    * Add cut off in BE for unathorized viewing of answers
    
    * update image tag version to 1.1.3
    
    ---------
    
    Co-authored-by: panagiotis-p <116631338+panagiotis-p@users.noreply.github.com>

--- libs/intention-analytics/src/lib/interceptors/transform-answers.interceptor.ts
+++ libs/intention-analytics/src/lib/interceptors/transform-answers.interceptor.ts
@@ -1,19 +1,30 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { mergeMap } from 'rxjs/operators';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository, EntityManager } from '@mikro-orm/postgresql';
+import { Question } from '../entities';
 
 import { IAnswer, QuestionResponse } from '../interfaces';
+import { QuestionType } from '../constants';
 
 @Injectable()
 export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]> {
+    constructor(@InjectRepository(Question) private readonly questionRepo: EntityRepository<Question>) {}
     intercept(context: ExecutionContext, next: CallHandler): Observable<any[]> {
         return next.handle().pipe(
-            map((data) => {
+            mergeMap(async (data) => {
                 const finalResults: QuestionResponse[] = [];
 
                 //put all the question responses into one array
+                //add the date it was given at in order to group by answer or by date
                 const allResponses: Record<string, any>[] = [].concat(
-                    ...data.map((answerRow: IAnswer) => answerRow.responses),
+                    ...data.map((answerRow: IAnswer) =>
+                        answerRow.responses.map((response: any) => ({
+                            ...response,
+                            date: answerRow.createdAt,
+                        })),
+                    ),
                 );
 
                 // based on the above, group questions into the final results array
@@ -23,13 +34,17 @@ export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]>
                     //check if result already exists in final results
                     const resultItem = finalResults.find(
                         (result: QuestionResponse) => result.questionId === questionResponse['questionId'],
-                    );
+                    ) as QuestionResponse;
 
                     //create array with submitted answers by user (text or option)
-                    const submittedAnswers: string[] =
+                    //add date to each answer in order to group by answer or by date
+                    const submittedAnswers: { response: string[]; date: string }[] =
                         'text' in questionResponse && questionResponse['text']
-                            ? [questionResponse['text']]
-                            : questionResponse['options'] ?? [];
+                            ? [{ response: questionResponse['text'], date: questionResponse['date'] }]
+                            : questionResponse['options'].map((option: string) => ({
+                                  response: option,
+                                  date: questionResponse['date'],
+                              })) ?? [];
 
                     //skip the iteration if neither text or options are found
                     if (!submittedAnswers.length) {
@@ -41,10 +56,15 @@ export class TransformAnswersInterceptor<T> implements NestInterceptor<T, any[]>
                     if (resultItem) {
                         resultItem.responses = [...resultItem.responses, ...submittedAnswers];
                     } else {
+                        const questionFromDb = await this.questionRepo.findOne({ id: questionResponse['questionId'] });
                         finalResults.push({
                             questionId: questionResponse['questionId'],
                             questionTitle: questionResponse['questionTitle'],
                             responses: submittedAnswers,
+                            questionType: questionFromDb ? (questionFromDb.type as QuestionType) : null,
+                            options: questionFromDb
+                                ? questionFromDb.options?.map((option: Record<string, any>) => option['text'])
+                                : [],
                         });
                     }
                 }
