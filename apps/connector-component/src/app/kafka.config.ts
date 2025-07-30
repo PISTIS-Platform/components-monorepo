51dc6d222269b9aa802a10473ecb87402ed9acb4
panagiotis-p <116631338+panagiotis-p@users.noreply.github.com>
2025-07-30 14:18:56 +0300

    remove external bootstrap servers env

--- apps/connector-component/src/app/kafka.config.ts
+++ apps/connector-component/src/app/kafka.config.ts
@@ -3,7 +3,6 @@ import { registerAs } from '@nestjs/config';
 export const KafkaConfig = registerAs('kafka', () => ({
     k8sDevContext: process.env.KAFKA_K8S_DEV_CONTEXT,
     bootstrapServers: process.env.KAFKA_BOOTSTRAP_SERVERS,
-    brokersPorts: process.env.KAFKA_BROKERS_PORTS,
     topicPartition: process.env.KAFKA_TOPIC_PARTITION || 1,
     topicReplicas: process.env.KAFKA_TOPIC_REPLICAS || 1,
     topicRetentionMs: process.env.KAFKA_TOPIC_RETENTION_MS || 604800000,
--- libs/kafka/src/lib/kafka.service.ts
+++ libs/kafka/src/lib/kafka.service.ts
@@ -370,11 +370,10 @@ export class KafkaService {
             password = (await this.getDecodedSecret(factoryName)) ?? '';
         }
 
-        const brokersPorts = this.config.get('kafka.brokersPorts')?.split(',') ?? [];
         return {
             username: factoryName,
             password,
-            bootstrapServers: brokersPorts.map((port: string) => `${factoryName}.pistis-market.eu:${port}`).join(','),
+            bootstrapServers: this.config.get<string>('kafka.bootstrapServers') ?? '',
             securityProtocol: 'SASL_PLAINTEXT',
             saslMechanism: 'SCRAM-SHA-512',
         };
