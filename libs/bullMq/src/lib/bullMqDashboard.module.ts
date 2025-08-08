89030440c00d5dc1092537a7b28dbcba9f3fdb3e
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2025-08-08 16:26:35 +0300

    Adding keycloak authorization in bull dashboard

--- libs/bullMq/src/lib/bullMqDashboard.module.ts
+++ libs/bullMq/src/lib/bullMqDashboard.module.ts
@@ -10,14 +10,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { HttpAdapterHost } from '@nestjs/core';
 import { Queue } from 'bullmq';
 import { NextFunction, Request, Response } from 'express';
-import * as jwt from 'jsonwebtoken'; // For JWT validatio
+// For JWT validatio
+import { verify } from 'jsonwebtoken';
+import * as jwks from 'jwks-rsa';
 
+// import { Issuer } from 'openid-client';
 import { CLIENT_SYNC } from './bullMq.constants';
 import { BullMqModule } from './bullMq.module';
 
 @Module({})
 export class BullmqDashboardModule implements OnModuleInit {
     private readonly logger = new Logger(BullmqDashboardModule.name);
+    private jwksClient: jwks.JwksClient | undefined;
 
     constructor(
         private readonly httpAdapterHost: HttpAdapterHost,
@@ -25,7 +29,15 @@ export class BullmqDashboardModule implements OnModuleInit {
         @InjectQueue(CLIENT_SYNC) private readonly clientSyncQueue: Queue, // Add other queues here if you want them displayed in the dashboard: // @InjectQueue(QUEUE_NAMES.EMAIL) private readonly emailQueue: Queue, // @InjectQueue(QUEUE_NAMES.IMAGE_PROCESSING) private readonly imageProcessingQueue: Queue, // @InjectQueue(QUEUE_NAMES.REPORT_GENERATION) private readonly reportGenerationQueue: Queue,
     ) {}
 
-    private keycloakAuthMiddleware(req: Request, res: Response, next: NextFunction) {
+    private async keycloakAuthMiddleware(req: Request, _res: Response, next: NextFunction) {
+        this.jwksClient = new jwks.JwksClient({
+            jwksUri: `https://auth.pistis-market.eu/realms/PISTIS/protocol/openid-connect/certs`,
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            cacheMaxAge: 600_000,
+        });
+
         const authHeader = req.headers.authorization;
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             // Throwing an exception here will be caught by NestJS's exception filter
@@ -33,35 +45,23 @@ export class BullmqDashboardModule implements OnModuleInit {
         }
 
         const token = authHeader.split(' ')[1];
-        // console.log(token);
-
         try {
-            // You must provide the Keycloak realm's public key here.
-            // The `jsonwebtoken` library will use it to verify the token's signature.
-            const publicKey = 'o6es3LPwnrghkX7e3urrwVnj5J7mvE8K'; // Get from config
-            const realm = 'PISTIS'; // Get from config
-            const authServerUrl = 'https://auth.pistis-market.eu'; // Get from config
+            // Extract the key id (kid) from the header of the token
+            const { kid } = JSON.parse(Buffer.from(token.split('.')[0], 'base64').toString());
 
-            if (!publicKey || !realm || !authServerUrl) {
-                throw new Error('Keycloak configuration is missing.');
-            }
+            // Retrieve the signing key from the issuer
+            const key = await this.jwksClient.getSigningKey(kid);
 
-            jwt.verify(token, publicKey, {
-                issuer: `${authServerUrl}/realms/${realm}`,
-                algorithms: ['RS256'],
-            });
+            // Get the public key
+            const publicKey = key.getPublicKey();
+            // Verify token, and return payload
+            verify(token, publicKey);
 
-            // You can add additional checks here, like for specific roles or scopes:
-            // const decodedPayload: any = jwt.decode(token);
-            // if (!decodedPayload.realm_access.roles.includes('admin')) {
-            //   throw new UnauthorizedException('Insufficient permissions.');
-            // }
-
-            next(); // Authentication successful, pass to the next middleware
-        } catch (err) {
-            console.log(err);
-            this.logger.error('JWT validation failed:', err.message);
-            throw new UnauthorizedException('Invalid or expired token.');
+            next();
+        } catch (err: any) {
+            // If anything went wrong, log the reason
+            console.log(err.message);
+            throw new UnauthorizedException('Invalid token');
         }
     }
 
@@ -92,27 +92,7 @@ export class BullmqDashboardModule implements OnModuleInit {
             serverAdapter,
         });
 
-        // const keycloakConfig: any = {
-        //     clientId: '',
-        //     bearerOnly: true,
-        //     serverUrl: '',
-        //     realm: '',
-        //     credentials: {
-        //         secret: '',
-        //     },
-        // };
-
-        // const keycloak = new Keycloak({}, keycloakConfig);
-        // expressApp.use(keycloak.middleware());
-
-        // expressApp.use('/admin/queues', keycloak.protect(), serverAdapter.getRouter());
-
-        expressApp.use(
-            '/admin/queues',
-            // The middleware function needs to be bound to `this` to access class properties
-            this.keycloakAuthMiddleware.bind(this),
-            serverAdapter.getRouter(),
-        );
+        expressApp.use('/admin/queues', this.keycloakAuthMiddleware.bind(this), serverAdapter.getRouter());
         this.logger.log('Bull Dashboard mounted at /admin/queues');
     }
 
