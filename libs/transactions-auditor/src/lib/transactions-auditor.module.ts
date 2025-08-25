a7978e271425ae6c4e4a4baecd72ca29a4ec9ffa
panagiotis-p <116631338+panagiotis-p@users.noreply.github.com>
2025-08-25 10:48:40 +0300

    add component healthcheck endpoint

--- libs/transactions-auditor/src/lib/transactions-auditor.module.ts
+++ libs/transactions-auditor/src/lib/transactions-auditor.module.ts
@@ -1,13 +1,15 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 
+import { ComponentHealthController } from './component-health.controller';
 import { TransactionsAuditorController } from './transactions-auditor.controller';
 import { TransactionsAuditor } from './transactions-auditor.entity';
 import { TransactionsAuditorService } from './transactions-auditor.service';
 
 @Module({
-    imports: [MikroOrmModule.forFeature([TransactionsAuditor])],
-    controllers: [TransactionsAuditorController],
+    imports: [MikroOrmModule.forFeature([TransactionsAuditor]), TerminusModule],
+    controllers: [TransactionsAuditorController, ComponentHealthController],
     providers: [TransactionsAuditorService],
     exports: [],
 })
