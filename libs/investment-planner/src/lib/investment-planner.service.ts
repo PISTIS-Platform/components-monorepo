ea03d63c10457a91efbdfa51251000f7d76f84ed
Giannis Giannopoulos <89904471+Ggiannopoulos@users.noreply.github.com>
2025-10-15 15:32:04 +0300

    fix: comment out notifications (#53)

--- libs/investment-planner/src/lib/investment-planner.service.ts
+++ libs/investment-planner/src/lib/investment-planner.service.ts
@@ -2,8 +2,7 @@ import { EntityRepository } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { HttpService } from '@nestjs/axios';
 import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
-import { getHeaders, UserInfo } from '@pistis/shared';
-import { catchError, firstValueFrom, map, of, tap, throwError } from 'rxjs';
+import { UserInfo } from '@pistis/shared';
 
 import { CreateInvestmentPlanDTO } from './create-investment-plan.dto';
 import { InvestmentPlanner } from './entities/investment-planner.entity';
@@ -109,47 +108,47 @@ export class InvestmentPlannerService {
     }
 
     //TODO: check if we want notifications for this component
-    private readonly createNotification = async (message: string, assetId: string, user: UserInfo) => {
-        const notification = {
-            userId: user.id,
-            organizationId: user.organizationId,
-            type: 'asset_retrieved',
-            message: 'Asset retrieval finished',
-        };
-        const tokenData = {
-            grant_type: 'client_credentials',
-            client_id: this.options.clientId,
-            client_secret: this.options.secret,
-        };
-        return await firstValueFrom(
-            this.httpService
-                .post(`${this.options.authServerUrl}/realms/PISTIS/protocol/openid-connect/token`, tokenData, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    data: JSON.stringify(tokenData),
-                })
-                .pipe(
-                    map(({ data }) => data.access_token),
-                    map((access_token) =>
-                        this.httpService
-                            .post(
-                                `${this.options.notificationsUrl}/srv/notifications/api/notifications`,
-                                notification,
-                                {
-                                    headers: getHeaders(access_token),
-                                },
-                            )
-                            .subscribe((value) => value),
-                    ),
-                    tap((response) => this.logger.debug(response)),
-                    map(() => of({ message: 'Notification created' })),
-                    // Catch any error occurred during the notification creation
-                    catchError((error) => {
-                        this.logger.error('Error occurred during notification creation: ', error);
-                        return throwError(() => new BadRequestException('Error occurred during notification creation'));
-                    }),
-                ),
-        );
-    };
+    // private readonly createNotification = async (message: string, assetId: string, user: UserInfo) => {
+    //     const notification = {
+    //         userId: user.id,
+    //         organizationId: user.organizationId,
+    //         type: 'asset_retrieved',
+    //         message: 'Asset retrieval finished',
+    //     };
+    //     const tokenData = {
+    //         grant_type: 'client_credentials',
+    //         client_id: this.options.clientId,
+    //         client_secret: this.options.secret,
+    //     };
+    //     return await firstValueFrom(
+    //         this.httpService
+    //             .post(`${this.options.authServerUrl}/realms/PISTIS/protocol/openid-connect/token`, tokenData, {
+    //                 headers: {
+    //                     'Content-Type': 'application/x-www-form-urlencoded',
+    //                 },
+    //                 data: JSON.stringify(tokenData),
+    //             })
+    //             .pipe(
+    //                 map(({ data }) => data.access_token),
+    //                 map((access_token) =>
+    //                     this.httpService
+    //                         .post(
+    //                             `${this.options.notificationsUrl}/srv/notifications/api/notifications`,
+    //                             notification,
+    //                             {
+    //                                 headers: getHeaders(access_token),
+    //                             },
+    //                         )
+    //                         .subscribe((value) => value),
+    //                 ),
+    //                 tap((response) => this.logger.debug(response)),
+    //                 map(() => of({ message: 'Notification created' })),
+    //                 // Catch any error occurred during the notification creation
+    //                 catchError((error) => {
+    //                     this.logger.error('Error occurred during notification creation: ', error);
+    //                     return throwError(() => new BadRequestException('Error occurred during notification creation'));
+    //                 }),
+    //             ),
+    //     );
+    // };
 }
