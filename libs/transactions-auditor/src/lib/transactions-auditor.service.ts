8a76ad07b24ab28450ac6d7b00540824ea3a3e4a
Hermes J. Pappas <hjp@hjp.gr>
2025-10-17 11:14:09 +0300

    Get transactions only from the last 30 days

--- libs/transactions-auditor/src/lib/transactions-auditor.service.ts
+++ libs/transactions-auditor/src/lib/transactions-auditor.service.ts
@@ -25,10 +25,8 @@ export class TransactionsAuditorService {
 
     async retrieveByFactory(query: PaginateQuery, user: UserInfo): Promise<PaginateResponse<TransactionAuditorDTO>> {
         const qb = this.repo.createQueryBuilder();
-
         // whitelist of fields users are allowed to filter on
         const allowedFilterFields = ['assetName'];
-
         // sanitize the filter to only include allowed fields
         const sanitizedFilter: any = {};
         if (query.filter && typeof query.filter === 'object') {
@@ -39,19 +37,29 @@ export class TransactionsAuditorService {
             }
         }
 
+        // calculate date range for last 30 days
+        const today = new Date();
+        today.setHours(23, 59, 59, 999); // end of today
+        const thirtyDaysAgo = new Date();
+        thirtyDaysAgo.setDate(today.getDate() - 30);
+        thirtyDaysAgo.setHours(0, 0, 0, 0); // start of 30 days ago
+
         // build the secure query
         const conditions: any[] = [
             {
                 $or: [{ factoryBuyerId: user.organizationId }, { factorySellerId: user.organizationId }],
             },
+            {
+                createdAt: {
+                    $gte: thirtyDaysAgo,
+                    $lte: today,
+                },
+            },
         ];
-
         if (Object.keys(sanitizedFilter).length > 0) {
             conditions.push(sanitizedFilter);
         }
-
         qb.where({ $and: conditions });
-
         return new PageFactory(query, qb).create();
     }
 
