fe4ce5fc8a31d9ff419b8b70f652c4fd66af39cb
Giannis Giannopoulos <giannis.giannopoulos@suite5.eu>
2026-02-16 17:08:23 +0200

    Changes for data trasfer

--- libs/data-storage/src/lib/data-storage.service.ts
+++ libs/data-storage/src/lib/data-storage.service.ts
@@ -5,17 +5,16 @@ import axios from 'axios';
 import FormData from 'form-data';
 import { catchError, firstValueFrom, lastValueFrom, map, of } from 'rxjs';
 
-
 @Injectable()
 export class DataStorageService {
     private readonly logger = new Logger(DataStorageService.name);
     constructor(
         private readonly httpService: HttpService,
-        // @Inject(MODULE_OPTIONS_TOKEN) private options: DataStorageModuleOptions,
-    ) { }
+    ) // @Inject(MODULE_OPTIONS_TOKEN) private options: DataStorageModuleOptions,
+    {}
 
     private prepareUrl(factory: string) {
-        return `https://${factory}.pistis-market.eu/srv/factory-data-storage/api`
+        return `https://${factory}.pistis-market.eu/srv/factory-data-storage/api`;
     }
 
     async updateTableInStorage(assetId: string, results: any, token: string, factory: string) {
@@ -43,16 +42,23 @@ export class DataStorageService {
             this.logger.error('Update table in storage error:', err);
             throw new Error(`Update table in storage error: ${err}`);
         }
-
     }
 
     async createTableInStorage(results: any, token: string, factory: string) {
         try {
             return await firstValueFrom(
                 this.httpService
-                    .post(`${this.prepareUrl(factory)}/tables/create_table`, { data: results.data.data, metadata: { id: results.metadata.id[0] }, data_model: results.data_model }, {
-                        headers: getHeaders(token),
-                    })
+                    .post(
+                        `${this.prepareUrl(factory)}/tables/create_table`,
+                        {
+                            data: results.data.data,
+                            metadata: { id: results.metadata.id[0] },
+                            data_model: results.data_model,
+                        },
+                        {
+                            headers: getHeaders(token),
+                        },
+                    )
                     .pipe(
                         map(async (res) => {
                             return res.data;
@@ -68,7 +74,6 @@ export class DataStorageService {
             this.logger.error('Create table in storage error:', err);
             throw new Error(`Create table in storage error: ${err}`);
         }
-
     }
 
     async retrievePaginatedData(
@@ -77,12 +82,14 @@ export class DataStorageService {
         offset: number,
         limit: number,
         columnsForPagination: Record<string, null>,
-        factory: string
+        factory: string,
     ) {
         return await firstValueFrom(
             this.httpService
                 .post(
-                    `${this.prepareUrl(factory)}/tables/get_fields?asset_uuid=${assetUUID}&OFFSET=${offset}&LIMIT=${limit}`,
+                    `${this.prepareUrl(
+                        factory,
+                    )}/tables/get_fields?asset_uuid=${assetUUID}&OFFSET=${offset}&LIMIT=${limit}`,
                     {
                         column_names: columnsForPagination,
                     },
@@ -92,9 +99,9 @@ export class DataStorageService {
                     map(async (res) => {
                         return {
                             data: {
-                                rows: res.data.Data
-                            }
-                        }
+                                rows: res.data.Data,
+                            },
+                        };
                     }),
                     // Catch any error occurred during data retrieval
                     catchError((error) => {
@@ -135,25 +142,22 @@ export class DataStorageService {
             this.logger.error('Count rows error:', err);
             throw new Error(`Count rows error: ${err}`);
         }
-
     }
 
     async getColumns(uuid: string, token: string, factory: string) {
         try {
-            return await fetch(`${this.prepareUrl(factory)}/tables/get_table?asset_uuid=${uuid}&JSON_output=true`,
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            ).then((res) => res.json())
-                .then((response) => response)
+            return await fetch(`${this.prepareUrl(factory)}/tables/get_table?asset_uuid=${uuid}&JSON_output=true`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then((res) => res.json())
+                .then((response) => response);
         } catch (err) {
             console.error('Get columns error:', err);
             this.logger.error('Get columns error:', err);
             throw new Error(`Get columns error: ${err}`);
         }
-
     }
 
     // async transferFile(assetId: string, token: string, consumerPrefix: string, providerPrefix: string) {
@@ -209,21 +213,23 @@ export class DataStorageService {
         try {
             // Fetch the file as a Blob
             const fileResponse = await lastValueFrom(
-                this.httpService.get(`${this.prepareUrl(providerPrefix)}/files/get_file?asset_uuid=${assetId}`, {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                    responseType: 'arraybuffer'
-                }).pipe(
-                    map(async (res) => {
-                        return res.data;
-                    }),
-                    // Catch any error occurred during update
-                    catchError((error) => {
-                        this.logger.error('Data fetching from storage error:', error);
-                        return of({ error: 'Error fetching the file' });
-                    }),
-                ),
+                this.httpService
+                    .get(`${this.prepareUrl(providerPrefix)}/files/get_file?asset_uuid=${assetId}`, {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                        responseType: 'arraybuffer',
+                    })
+                    .pipe(
+                        map(async (res) => {
+                            return res.data;
+                        }),
+                        // Catch any error occurred during update
+                        catchError((error) => {
+                            this.logger.error('Data fetching from storage error:', error);
+                            return of({ error: 'Error fetching the file' });
+                        }),
+                    ),
             );
 
             return fileResponse;
@@ -236,9 +242,12 @@ export class DataStorageService {
     async createFile(data: any, filename: string, token: string, consumerPrefix: string): Promise<any> {
         try {
             const formData = new FormData();
-            const buffer = Buffer.from(data.data)
+            const buffer = Buffer.from(data.data);
             formData.append('file', buffer, { filename, contentType: 'application/octet-stream' });
             // POST the data to create a new file
+            console.log('formData', formData);
+            console.log('filename', filename);
+            console.log(`${this.prepareUrl(consumerPrefix)}/files/create_file`);
             const uploadResponse = await axios.post(`${this.prepareUrl(consumerPrefix)}/files/create_file`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
@@ -250,8 +259,7 @@ export class DataStorageService {
                 throw new Error(`Error uploading the file: ${uploadResponse.statusText}`);
             }
 
-            return uploadResponse.data
-
+            return uploadResponse.data;
         } catch (error) {
             console.error(`Error creating file: ${error}`);
             throw new Error(`Error creating file: ${error}`);
